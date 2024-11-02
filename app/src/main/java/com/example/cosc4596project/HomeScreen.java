e7482875a5479c256fcac9ed1fce94182fb55984
puroy3 <puroy@algomau.ca>
2024-11-02 12:57:55 -0400

    Added functionality to move from home screen to login and signup screens.

--- app/src/main/java/com/example/cosc4596project/HomeScreen.java
+++ app/src/main/java/com/example/cosc4596project/HomeScreen.java
@@ -1,6 +1,9 @@
 package com.example.cosc4596project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeScreen extends AppCompatActivity {
+
+    Button signUpButton1;
+    Button loginButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,24 @@ public class HomeScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        signUpButton1=findViewById(R.id.signUpButton1);
+        signUpButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(view.getContext(), SignUp.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+        loginButton1=findViewById(R.id.loginButton1);
+        loginButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(view.getContext(), MainActivity.class);
+                view.getContext().startActivity(intent);
+            }
         });
     }
 }
