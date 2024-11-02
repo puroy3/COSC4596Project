dfa3a14b375a3fe9adbc5eb631cb5b8cb58b39f4
puroy3 <puroy@algomau.ca>
2024-11-02 12:37:07 -0400

    Add Project Code

---
+++ app/src/main/java/com/example/cosc4596project/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.cosc4596project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button homeButton1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        homeButton1=findViewById(R.id.homeButton1);
+        homeButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(view.getContext(), HomeScreen.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+    }
+}
